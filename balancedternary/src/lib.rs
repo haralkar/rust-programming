79f6353a0cb1d3fd40069f8b3d923b78985f56d4
Haraldur Karlsson <haralkar@gmail.com>
2017-02-26 21:45:39 +0100

    zero and one

--- balancedternary/src/lib.rs
+++ balancedternary/src/lib.rs
@@ -1,6 +1,12 @@
 
-pub fn as_bal_ter(_frm: i32) -> String {
-    String::from("0")
+
+
+pub fn as_bal_ter(frm: i32) -> String {
+    if frm == 0 {
+        return String::from("0")
+    }
+
+    return String::from("+")
 }
 
 #[cfg(test)]
@@ -8,8 +14,12 @@ mod tests {
     use super::as_bal_ter;
 
 #[test]
-fn it_works() {
+fn null_is() {
     assert_eq!(as_bal_ter(0), "0");
+}
+#[test]
+fn one_is_plus() {
+    assert_eq!(as_bal_ter(1), "+");
 }
 
 }
