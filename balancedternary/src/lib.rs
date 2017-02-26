9338e4a90141cd5a4330d29b2ee1ab70313246f6
Haraldur Karlsson <haralkar@gmail.com>
2017-02-26 22:56:07 +0100

    higher power

--- balancedternary/src/lib.rs
+++ balancedternary/src/lib.rs
@@ -1,24 +1,29 @@
 
-
-
-pub fn as_bal_ter(frm: i32) -> String {
-    helper(frm, 1)
-}
-pub fn helper(frm: i32, mul: i32) -> String {
+pub fn helper(frm: &mut i32, mul: i32) -> String {
+    println!("+  {:?}", (&frm, &mul));
     let mut out = if mul < frm.abs() {
         helper(frm, mul*3)
     } else {
         String::from("")
     };
+    println!(" + {:?}", (&frm, &mul));
 
-    out.push_str(if frm == 0 {
-        "0"
-    } else if frm > 0 {
+    out.push_str(if *frm < 0 {
+        *frm += mul;
+        "-"
+    } else if *frm > 0 {
+        *frm -= mul;
         "+"
     } else {
-        "-"
+        "0"
     });
+    println!("  +{:?}", (&frm, &mul, &out));
     out
+}
+pub fn as_bal_ter(frm: i32) -> String {
+    println!("\nfrom, multipl");
+    let mut from = frm;
+    helper(&mut from, 1)
 }
 
 #[cfg(test)]
@@ -37,10 +42,10 @@ fn one_is_plus() {
 fn minus_is_dash() {
     assert_eq!(as_bal_ter(-1), "-");
 }
-/*
+//*
 #[test]
 fn three_is_ten() {
-    assert_eq!(as_bal_ter(3), "10");
+    assert_eq!(as_bal_ter(3), "+0");
 }
 // */
 
